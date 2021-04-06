51e5314443fe09c9d5ba70845592beb954d2d522
jcerb <brt.jst@gmail.com>
2021-04-05 19:19:54 -0700

    adjust user model

--- models/User.js
+++ models/User.js
@@ -20,14 +20,6 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
-    },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -42,6 +34,10 @@ User.init(
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
+      beforeUpdate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+       }  
     },
     sequelize,
     timestamps: false,
