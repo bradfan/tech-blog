bb27472c937f57e1c5297d77c93f8ca97610b41b
jcerb <brt.jst@gmail.com>
2021-04-05 19:14:09 -0700

    add reply model

--- models/reply.js
+++ models/reply.js
@@ -1,16 +1,29 @@
 const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model {
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
-  }
-}
+
+class Reply extends Model {}
 
 Reply.init(
     {
-        // build new models here
+        id:{
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        name:{
+          type: DataTypes.STRING,
+          allowNull: false,  
+        },
+        post:{
+          type: DataTypes.STRING,
+          allowNull: false, 
+         },
+         user_id: {
+           type: DataTypes.INTEGER,
+           foreignKey: true,
+         }
     }
 )
 
