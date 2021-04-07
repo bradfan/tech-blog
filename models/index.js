1fe925c26c260c827679fe8713c090be0b3510a3
jcerb <brt.jst@gmail.com>
2021-04-06 20:22:09 -0700

    degug code

--- models/index.js
+++ models/index.js
@@ -1,3 +1,7 @@
 const User = require('./User');
+// const Post = require('./post');
+// const Reply = require('./reply');
 
 module.exports = { User };
+// module.exports = { Post };
+// module.exports = { Reply };
