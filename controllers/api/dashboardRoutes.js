22245e5e9d7c55c525a8dc4f7c355eb525615c69
jcerb <brt.jst@gmail.com>
2021-04-07 20:27:36 -0700

    begin dashboard routes

--- controllers/api/dashboardRoutes.js
+++ controllers/api/dashboardRoutes.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { Post } = require('../../models');
 const withAuth = require('../utils/auth');
+const { post } = require('./userRoutes');
 
 
-
+router.get("/dashboard", async(req,res) => {
+    try{
+        const postInput = await Post.findAll(req.body);
+        res.render("dashboard")
+    }catch (err) {
+        res.status(500).json(err);
+    }
+    
+    
+})
 // // dashboard.js works with dashboard.handlebars and dashboard route.js as well as post and reply models
